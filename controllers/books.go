3921a555ff6f0401a5e707c84c3b291b3fbd684f
Guilherme Barros <hello@gbarros.dev>
2020-12-19 11:52:57 -0300

    update book

--- controllers/books.go
+++ controllers/books.go
@@ -13,6 +13,11 @@ type CreateBookInput struct {
 	Author	string	`json:"author" bingind:"required"`
 }
 
+type UpdateBookInput struct {
+  Title  string `json:"title"`
+  Author string `json:"author"`  
+}
+
 // GET /books
 // Get all books
 func FindBooks(c *gin.Context) {
@@ -48,6 +53,27 @@ func CreateBook(c *gin.Context) {
 	// Create book
 	book := models.Book{Title: input.Title, Author: input.Author}
 	models.DB.Create(&book)
+
+	c.JSON(http.StatusOK, gin.H{"data": book})
+}
+
+// PATCH /books/:id
+// Update a book
+func UpdateBook(c *gin.Context) {
+	var book models.Book
+
+	if err := models.DB.Where("id = ?", c.Param("id")).First(&book).Error; err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Record not found!"})
+		return
+	}
+
+	var input UpdateBookInput
+	if err := c.ShouldBindJSON(&input); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	models.DB.Model(&book).Updates(input)
 
 	c.JSON(http.StatusOK, gin.H{"data": book})
 }
